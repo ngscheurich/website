6b6936a26b6dba3a86e5c9d4066c96f0455fbc65
N. G. Scheurich <nick@scheurich.me>
2018-01-02 20:24:26 -0600

    Add favicons

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,11 @@ import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
 
-import avatar from "../images/avatar.png";
+import appleTouchIcon from "../static/favicons/apple-touch-icon.png";
+import favicon16 from "../static/favicons/favicon-16x16.png";
+import favicon32 from "../static/favicons/favicon-32x32.png";
+import avatar from "../static/images/avatar.png";
+
 const title = "The Personal Website of Mr. Nicholas Gunther Scheurich";
 const description = `Scholar of arcane mysteries such as computer programming,
    game design, and the Vim text editor.`;
@@ -13,11 +17,16 @@ const TemplateWrapper = ({ children }) => (
     <Helmet
       title={title}
       meta={[
-        { name: "description", content: { description } },
+        { name: "description", content: description },
         { name: "og:url", content: `https://www.ngscheurich.com/` },
         { name: "og:title", content: title },
         { name: "og:description", content: description },
         { name: "og:image", content: avatar }
+      ]}
+      link={[
+        { rel: "apple-touch-icon", sizes: "180x180", href: favicon16 },
+        { rel: "icon", type: "image/png", sizes: "32x32", href: favicon16 },
+        { rel: "icon", type: "image/png", sizes: "16x16", href: favicon16 }
       ]}
     />
 
