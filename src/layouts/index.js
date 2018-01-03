88c073bd1d5d9c251e956ca6114c9701e93d2823
N. G. Scheurich <nick@scheurich.me>
2018-01-03 02:03:14 -0600

    Use simpler favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,12 +3,10 @@ import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
 
-import appleTouchIcon from "../static/favicons/apple-touch-icon.png";
-import favicon16 from "../static/favicons/favicon-16x16.png";
-import favicon32 from "../static/favicons/favicon-32x32.png";
+import favicon from "../static/favicon.ico";
 import avatar from "../static/images/avatar.png";
 
-const title = "The Personal Website of Mr. Nicholas Gunther Scheurich";
+const title = "The Personal Website of Mr. N. G. Scheurich";
 const description = `Scholar of arcane mysteries such as computer programming,
    game design, and the Vim text editor.`;
 
@@ -23,11 +21,7 @@ const TemplateWrapper = ({ children }) => (
         { name: "og:description", content: description },
         { name: "og:image", content: avatar }
       ]}
-      link={[
-        { rel: "apple-touch-icon", sizes: "180x180", href: favicon16 },
-        { rel: "icon", type: "image/png", sizes: "32x32", href: favicon16 },
-        { rel: "icon", type: "image/png", sizes: "16x16", href: favicon16 }
-      ]}
+      link={[{ rel: "icon", href: favicon }]}
     />
 
     {children()}
