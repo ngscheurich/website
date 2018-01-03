d82bd69782513d25c3b18892fe3836b9c0c8690f
N. G. Scheurich <nick@scheurich.me>
2018-01-03 13:10:12 -0600

    Add spacing after emojis

--- src/components/index/ProjectList.js
+++ src/components/index/ProjectList.js
@@ -8,31 +8,31 @@ export default () => (
 
     <Project
       url="/projects/soundtrack-of-2017"
-      title="🎧 Soundtrack of 217"
+      title="🎧&nbsp;&nbsp;Soundtrack of 217"
       description="What my 2017 sounded like, according to Spotify"
     />
 
     <ProjectExternal
       url="https://github.com/ngscheurich/http_find"
-      title="🔎 HTTP Find"
+      title="🔎&nbsp;&nbsp;HTTP Find"
       description="Searches the contents of an HTTP response for a specified term or pattern"
     />
 
     <ProjectExternal
       url="https://github.com/ngscheurich/generator-phaser-ts"
-      title="👾 Phaser + TypeScript Generator"
+      title="👾&nbsp;&nbsp;Phaser + TypeScript Generator"
       description=" Provides a handy jumpstart for a Phaser project with a TypeScript codebase"
     />
 
     <ProjectExternal
       url="https://github.com/ngscheurich/metallum"
-      title="💀 Metallum"
+      title="💀&nbsp;&nbsp;Metallum"
       description="Displays random bands from Encyclopaedia Metallum: The Metal Archive "
     />
 
     <ProjectExternal
       url="https://github.com/ngscheurich/boxcar"
-      title="🚋 Boxcar"
+      title="🚋&nbsp;&nbsp;Boxcar"
       description="Opinionated Rails-based CMS stack with modern frontend tooling"
     />
   </div>
