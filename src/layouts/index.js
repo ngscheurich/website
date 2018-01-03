0b05d5ff2215b6e48ddee50e6bec74744b0e2d71
N. G. Scheurich <nick@scheurich.me>
2018-01-02 19:49:23 -0600

    Add basic <meta> tags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,12 +3,24 @@ import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
 
+import avatar from "../images/avatar.png";
+const title = "The Personal Website of Mr. Nicholas Gunther Scheurich";
+const description = `Scholar of arcane mysteries such as computer programming,
+   game design, and the Vim text editor.`;
+
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="N. G. Scheurich"
-      meta={[{ name: "description", content: "Sample" }]}
+      title={title}
+      meta={[
+        { name: "description", content: { description } },
+        { name: "og:url", content: `https://www.ngscheurich.com/` },
+        { name: "og:title", content: title },
+        { name: "og:description", content: description },
+        { name: "og:image", content: avatar }
+      ]}
     />
+
     {children()}
   </div>
 );
