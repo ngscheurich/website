94ac94fbe74e6b29d03d1cc3b2aff9106005af74
N. G. Scheurich <nick@scheurich.me>
2018-01-05 16:55:52 -0600

    Set less metadata in layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,7 +4,6 @@ import Link from "gatsby-link";
 import Helmet from "react-helmet";
 
 import favicon from "../static/favicon.ico";
-import avatar from "../static/images/avatar.png";
 
 const title = "The Personal Website of Mr. N. G. Scheurich";
 const description = `Scholar of arcane mysteries such as computer programming,
@@ -12,17 +11,7 @@ const description = `Scholar of arcane mysteries such as computer programming,
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet
-      title={title}
-      meta={[
-        { name: "description", content: description },
-        { name: "og:url", content: `https://www.ngscheurich.com/` },
-        { name: "og:title", content: title },
-        { name: "og:description", content: description },
-        { name: "og:image", content: avatar }
-      ]}
-      link={[{ rel: "icon", href: favicon }]}
-    />
+    <Helmet title={title} link={[{ rel: "icon", href: favicon }]} />
 
     {children()}
   </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,42 @@
 import React from "react";
+import Helmet from "react-helmet";
 import Bio from "../components/index/Bio";
 import ProjectList from "../components/index/ProjectList";
 import BlogPostList from "../components/index/PostSummaryList";
 
 import tachyons from "tachyons";
 import "../styles/index.css";
+import avatar from "../static/images/avatar.png";
 
 export default ({ data }) => {
   const { edges: posts } = data.allMarkdownRemark;
 
   return (
-    <div className="index black-80 flex-l w-100-l vh-100-l fixed-l">
-      <div className="pa4 pa5-ns bb bb-0-l br-l b--black-10">
-        <header className="measure pb3" role="banner">
-          <Bio />
-        </header>
+    <div>
+      <Helmet
+        meta={[
+          { name: "description", content: description },
+          { name: "og:url", content: `https://www.ngscheurich.com/` },
+          { name: "og:title", content: title },
+          { name: "og:description", content: description },
+          { name: "og:image", content: avatar }
+        ]}
+      />
+      <div className="index black-80 flex-l w-100-l vh-100-l fixed-l">
+        <div className="pa4 pa5-ns bb bb-0-l br-l b--black-10">
+          <header className="measure pb3" role="banner">
+            <Bio />
+          </header>
 
-        <main className="measure" role="main">
-          <ProjectList />
-        </main>
-      </div>
+          <main className="measure" role="main">
+            <ProjectList />
+          </main>
+        </div>
 
-      <div className="flex-auto overflow-y-scroll">
-        <div className="measure-wide pa4 pa5-ns">
-          <BlogPostList posts={posts} />
+        <div className="flex-auto overflow-y-scroll">
+          <div className="measure-wide pa4 pa5-ns">
+            <BlogPostList posts={posts} />
+          </div>
         </div>
       </div>
     </div>
